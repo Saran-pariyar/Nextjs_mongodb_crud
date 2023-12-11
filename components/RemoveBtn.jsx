68f4ae6340641c630b0ddd3efcb460cb3ec1a8cc
Saran Pariyar <spariyar05@gmail.com>
2023-12-11 07:49:13 +0545

    Made components, next:make functional, connect to mongoDB

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+export default function RemoveBtn() {
+  return (
+    <button  className="text-red-400">
+      <HiOutlineTrash size={24} />
+    </button>
+  )
+}

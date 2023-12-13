490c1eee92efe03cd0350153e1c519d65b4b3f34
Saran Pariyar <spariyar05@gmail.com>
2023-12-13 14:18:51 +0545

    Made edit functionality

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,10 +1,26 @@
 import EditTopicForm from '@/components/EditTopicForm'
-import React from 'react'
 
-export default function EditTopic ()  {
-  return (
-    <div>
-<EditTopicForm />
-    </div>
-  )
+const getTopicById = async (id) =>{
+  try{
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`,{
+      cache:"no-store"
+    })
+    if(!res.ok){
+      throw new Error("Failed to fetch topic")
+    }
+    return res.json()
+  }
+  catch(error){
+console.log(error);
+  }
+}
+
+export default async function EditTopic ({params})  {
+
+  const {id} = params;
+  //we're destructuring {topic} because we'll receive an object and it will have "topic" property and that topic property will contain all data
+const {topic} = await getTopicById(id);
+const {title, description} = topic;
+
+return <EditTopicForm id={id} title={title} description={description} />;
 }

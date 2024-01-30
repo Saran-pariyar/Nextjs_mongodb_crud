17a00f7d02cebd3863396535961dee1201ec444d
Saran Pariyar <spariyar05@gmail.com>
2024-01-30 09:17:12 +0545

    added optional chaining for vercel server log

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -23,7 +23,7 @@ const {topics} = await getTopics()
 
     return (
         <>
-        {topics.map(t=>(
+        {topics?.map(t=>(
         <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start" key={t._id}>
             <div>
                 <h2 className="font-bold text-2xl">{t.title}</h2>
